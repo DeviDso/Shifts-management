652c7b7808a3a4909aae5ae83cb82b7d247bb2b9
David <dsadeckas@gmail.com>
2018-08-18 14:58:25 +0300

    dotenv integration

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,9 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import dotenv from 'dotenv'
+dotenv.config({path: './.env'})
+
 // import FullCalendar from 'vue-full-calendar'
 // import moment from 'moment'
 import UIkit from 'uikit'
@@ -12,17 +15,15 @@ import FullCalendar from 'vue-full-calendar'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 
-axios.defaults.baseURL = 'http://scheduleapi.test/api/';
-// axios.defaults.baseURL = 'http://senoliai.desam.lt/api/'
-axios.defaults.headers.common['X-Auth-Token'] = '35A3A9ED6B7FCAD877A64BB0C9E6A28928240B5365ABDDC926169F449832E2F0'
+axios.defaults.baseURL = process.env.API_URL
+axios.defaults.headers.common['API-KEY'] = process.env.API_KEY
+
 Vue.http = Vue.prototype.$http = axios
 Vue.prototype.$uikit = UIkit
 Vue.config.productionTip = false
 
 Vue.use(FullCalendar)
-// Vue.use(moment)
 
-/* eslint-disable no-new */
 new Vue({
   components: { App },
   router,
