8269e095a6c83cff7cb93e444c5690ca5d66f69d
David <dsadeckas@gmail.com>
2018-08-29 06:46:32 +0300

    API Url

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -9,13 +9,8 @@ import FullCalendar from 'vue-full-calendar'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 
-// axios.defaults.baseURL = process.env.API_URL
-// axios.defaults.headers.common['API-KEY'] = process.env.API_KEY
-
-// axios.defaults.baseURL = 'https://senoliai.desam.lt/api'
-axios.defaults.baseURL = 'http://scheduleapi.test/api'
-axios.defaults.headers.common['API-KEY'] = '7ACA0D339949799D07AB0FD6FA784E25C97BA36D71B550B9AF20BC1EBA285507'
-
+axios.defaults.baseURL = process.env.API_URL
+axios.defaults.headers.common['API-KEY'] = process.env.API_KEY
 
 Vue.http = Vue.prototype.$http = axios
 Vue.prototype.$uikit = UIkit
